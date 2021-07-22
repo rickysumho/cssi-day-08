be04d6e395eb86c55750c289cdecff1c7aaaa89b
Ricky Ho <horicky78@gmail.com>
2021-07-22 16:46:59 +0000

    Added Firebase database reference

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -1,5 +1,9 @@
-const clickBtn = document.querySelector('#viewMsg');
-
 function getMessages() {
-    console.log("working!!!");
+    const passcode = document.querySelector('#passcode');
+    const messagesRef = firebase.database().ref();
+    messagesRef.on('value', (snapshot) => {
+        const data = snapshot.val();
+        console.log(data);
+    })
+    console.log("button pressed:", passcode.value);
 }
