5e8c7fd9a4c3d09a7eab6a5b01155dd67cbb62ce
Ricky Ho <horicky78@gmail.com>
2021-07-22 17:07:21 +0000

    Viewing messages working!

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -1,3 +1,5 @@
+
+
 function getMessages() {
     const passcode = document.querySelector('#passcode');
     const messagesRef = firebase.database().ref();
@@ -6,6 +8,11 @@ function getMessages() {
         console.log(data);
         for (let key in data) {
             console.log(key, data[key]);
+            if (key === passcode.value) {
+                console.log("match found");
+                const message = document.getElementById('message');
+                message.innerText = data[key];
+            }
         }
     })
     console.log("button pressed:", passcode.value);
